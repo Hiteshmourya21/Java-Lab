0d4f3dd839ea29444bb4beffffce7a8edae91f58
Hiteshmourya21 <143187032+Hiteshmourya21@users.noreply.github.com>
2023-09-06 11:52:16 +0530

    Add files via upload

---
+++ WrapperExample1.java
@@ -0,0 +1,11 @@
+//This is a program for auto boxing
+public class WrapperExample1
+{
+	public static void main(String[] args) {
+	    int a= 20;
+	    Integer i = Integer.valueOf(a);
+	    Integer j = a;
+	   
+		System.out.println(a+" "+i+" "+j);
+	}
+}
