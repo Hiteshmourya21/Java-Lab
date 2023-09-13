fd113c23ec17ea094e6a2eaa78a7855a84616e8d
Hiteshmourya21 <143187032+Hiteshmourya21@users.noreply.github.com>
2023-09-13 12:15:15 +0530

    Add files via upload

---
+++ CheckEmptyString.java
@@ -0,0 +1,16 @@
+
+//to check wether the string is empty or not
+
+public class CheckEmptyString
+{
+	public static void main(String[] args) {
+	    //create a string
+	    String greet  = "Hello! World";
+		System.out.println("String : "+ greet);
+		
+	  // Check empty String
+	  boolean b = greet.isEmpty();
+		System.out.println(b);
+		System.out.println(greet.isEmpty());
+	}
+}
