c001b637192dd2747689b75aa2fbecc08a07b714
Hiteshmourya21 <143187032+Hiteshmourya21@users.noreply.github.com>
2023-09-13 11:38:30 +0530

    Add files via upload

---
+++ StringBasic.java
@@ -0,0 +1,13 @@
+
+
+public class StringBasic
+{
+	public static void main(String[] args) {
+	    String first = "Java";
+	    String s = new String("Python");
+	    String third = "Javascipt";
+		System.out.println(first);
+		System.out.println(s);
+		System.out.println(third);
+	}
+}
