d42a43679fd7d51c13b11e0eb0fcd34d6a52ea14
Hiteshmourya21 <143187032+Hiteshmourya21@users.noreply.github.com>
2023-09-13 11:42:12 +0530

    Add files via upload

---
+++ StringLength.java
@@ -0,0 +1,14 @@
+
+
+public class StringLength
+{
+	public static void main(String[] args) {
+	    //create a string
+	    String greet  = "Hello! World";
+		System.out.println("String : "+ greet);
+		
+	  // get lenght of String
+	  int length = greet.length();
+		System.out.println("lenght = " + length);
+	}
+}
