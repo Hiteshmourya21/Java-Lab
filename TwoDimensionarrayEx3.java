68d57b5f93d8c85c51970e5062f0e1a3826fc2bf
Hiteshmourya21 <143187032+Hiteshmourya21@users.noreply.github.com>
2023-10-04 12:32:38 +0530

    Add files via upload

---
+++ TwoDimensionarrayEx3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class TwoDimensionarrayEx3{
+
+	public static void main(String[] args) {
+	    Scanner sc =new Scanner(System.in);
+	    //System.out.println("Enter row of array : ");
+	    //int n = sc.nextInt();
+	    // System.out.println("Enter column of array : ");
+	   // int m = sc.nextInt();
+	    String[][] arr={{"Mr.","Mrs.","Ms."},{"Kumar"}};
+	    System.out.println(arr[0][0] + arr[1][0]);
+	    System.out.println(arr[0][1] + arr[1][0]);
+	    System.out.println(arr[0][2] + arr[1][0]);
+	}
+}
